eabdb6401bdc5d83c823eb1e72ce008aad5d396f
szabomarcel <szabomarcell34@gmail.com>
2023-03-20 10:47:03 +0100

    main

---
+++ CimleteresOOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimleteresOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Írjon be egy összeget:");
+            int penz = Convert.ToInt32(Console.ReadLine());
+            int cimlet_helye = 0;
+            while (penz >= 0 && penz <= 20000)
+            {
+                int mennyiseg = penz / penzfajtak(cimlet_helye);
+                penz = penz % penzfajtak(cimlet_helye);
+                Console.WriteLine("{0} darab {1} Ft-os", mennyiseg);
+                cimlet_helye++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CimleteresOOP/penzerme.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CimleteresOOP
+{
+    internal class penzerme
+    {
+        struct penz
+        {
+            public int id; // Penzerme neve
+        }
+        internal class PontHat
+        {
+            // Osztályváltozók
+            private int penz;
+            static string[] penzfajtak = [ 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 ];
+            private int penzermek = 0;
+
+            //Konstruktor
+            public penzerme(int p)
+            {
+                this.id = p;
+            }
+
+            // Eredmények átadó metódusok
+            public string getPenzerme()
+            {
+                return 
+            }
+            public void szamitasa()
+            {
+                // Kerekités számítás
+                setPenz();
+            }
+
+            private void setPenz()
+            {
+                if ( this.penz > 1 && this.penz < 200000)
+                {
+
+                    Math.round();
+                }
+                
+            }
+        }
+    }
+}
+
