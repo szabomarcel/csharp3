e765a779f7e42d5dd5bc0d1e8ffcb5c51c553465
szabomarcel <szabomarcell34@gmail.com>
2023-03-24 08:31:19 +0100

    main

--- szamokbekereseOOP/Program.cs
+++ szamokbekereseOOP/Program.cs
@@ -15,26 +15,19 @@ namespace szamokbekereseOOP
             Console.WriteLine();
             int hossz = n.Length;
             int osszeg = 0;
-            for (int i = 0; i < hossz; i++)
-            {
-                osszeg = osszeg + Convert.ToInt16(n[i]) - 48;
-            }
-            if (osszeg % 3 == 0)
-                Console.WriteLine("A szám osztható 3-mal.");
-            else Console.WriteLine("A szám nem osztható 3-mal.");
-            if (osszeg % 9 == 0)
-                Console.WriteLine("A szám osztható 9-cel.");
-            else Console.WriteLine("A szám nem osztható 9-cel.");
-            if (hossz > 1)
-                if (hossz > 1)
-                {
-                    if ((Convert.ToInt16(n[hossz - 2] - 48) * 10 + Convert.ToInt16(n[hossz - 1]) - 48) % 4 == 0)
-                        Console.WriteLine("A szám osztható 4-gyel.");
-                    else Console.WriteLine("A szám nem osztható 4-gyel.");
-                }
-                else if ((Convert.ToInt16(n[0]) - 48) % 4 == 0)
-                    Console.WriteLine("A szám osztható 4-gyel.");
-                else Console.WriteLine("A szám nem osztható 4-gyel.");
+            
+            Console.WriteLine("A szám osztható 3-mal.");
+            Console.WriteLine("A szám nem osztható 3-mal.");
+            
+            Console.WriteLine("A szám osztható 9-cel.");
+            Console.WriteLine("A szám nem osztható 9-cel.");
+            
+                    
+            Console.WriteLine("A szám osztható 4-gyel.");
+            Console.WriteLine("A szám nem osztható 4-gyel.");
+            
+            Console.WriteLine("A szám osztható 4-gyel.");
+            Console.WriteLine("A szám nem osztható 4-gyel.");
             Console.ReadLine();
         }
     }
--- szamokbekereseOOP/szamok.cs
+++ szamokbekereseOOP/szamok.cs
@@ -13,8 +13,36 @@ namespace szamokbekereseOOP
     internal class szamok
     {
         // Osztályválétozók
-        string n;
-        int hossz;
-        int osszeg;
+        private string n;
+        private int hossz;
+        private int osszeg;
+
+        public szamok() { }
+
+        public szamok(string n, int hossz)
+        {
+            this.n = n;
+            this.hossz = hossz;
+        }
+        public void szamitas()
+        {
+            setszambe();
+        }
+
+        private void setszambe()
+        {
+            for (int i = 0; i < hossz; i++)
+            {
+                osszeg = osszeg + Convert.ToInt16(n[i]) - 48;
+            }
+            if (osszeg % 3 == 0) ;
+            if (osszeg % 9 == 0) ;
+            if (hossz > 1)
+                if (hossz > 1)
+                {
+                    if ((Convert.ToInt16(n[hossz - 2] - 48) * 10 + Convert.ToInt16(n[hossz - 1]) - 48) % 4 == 0) ;
+                    else if ((Convert.ToInt16(n[0]) - 48) % 4 == 0) ;
+                }
+        }
     }
 }
