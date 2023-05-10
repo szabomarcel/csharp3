b14cc56e5cf1f7cab06b8b7793e34a7dbca64c0d
szabomarcel <szabomarcell34@gmail.com>
2023-05-10 20:58:31 +0200

    main

---
+++ BaratokOOP/Baratok.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    internal class Baratok
+    {
+        // Konstruktor
+        struct barat
+        {
+            public string nev;
+            public int eletkor;
+        }
+        //  Adatok beolvasása fájlból vagy generálása a programban, majd elhelyezése listában.
+        StreamWriter f = new StreamWriter(@"C:\Users\Lenovo\Source\Repos\csharp\BaratokOOP\bin\Debug\fajlnev");
+        List<int> list = new List<int>();
+        // Új barát felvétele a listába
+
+        
+        // Barát törlése a listából név alapján
+       
+
+        // Annak ellenőrzése, hogy egy személy szerepel-e a listában
+        
+
+        // Lista kiíratása fájlba.A fájl neve: baratok.csv
+
+
+        // 1.-B Barátok osztály bővítése
+        // Táblázatba kiíratja a szülő osztályt + bővítés
+
+
+
+        /*
+        * 1.-C Barátok osztály bővítése
+        * Lapozás előre-hátra képernyőnként 20 rekord kiírással kalkulálva!
+        * A lapozást a PageUp és PageDown gombokkal lehessen megvalósítani!
+        * Ezenfelül a Home billentyűvel lehessen az első, az End billentyűvel az utolsó lapra ugorni!
+        */
+        
+        
+        
+        /*
+        * 1.-D Barátok osztály bővítése
+        * A szülő osztályt új metódussal bővítjük, amely a lista alapján határozza meg,
+        * hogy van-e elég olyan barátunk, akikkel egy születésnapi bulit tudunk összehozni!
+        * Ehhez olyan barátokra van szükségünk, akik életkora hasonló a miénkhez(+/- 3 év) és bulizási hajlama legalább 5-ös skálaértékű.A buli minimális létszáma 10 fő.
+        * Amennyiben van elég barátunk, írjuk ki a neveiket a buli.csv fájlba!
+        */
+
+
+
+    }
+}
---
+++ BaratokOOP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> NEVEK = new List<string>();
+            NEVEK.Add(@"C:\Users\Lenovo\Source\Repos\csharp\BaratokOOP\bin\Debug\fajlnev");
+            for (int i = 0; i < NEVEK.Count; i++)
+            {
+                Console.Write(NEVEK[i] + ", ");
+            }
+            // Insert
+            NEVEK.Insert(2, "Új fiú");
+
+            // Remove
+            NEVEK.Remove(@"C:\Users\Lenovo\Source\Repos\csharp\BaratokOOP\bin\Debug\fajlnev");
+            NEVEK.RemoveAt(1);
+            // Clear
+            NEVEK.Clear();
+
+            /*
+            Baratok a = new Baratok();
+            StreamWriter f = new StreamWriter(@"feladat2.text");
+            string[] s;
+            double atl = 0, sz = 0;
+            /*
+            while (g.Peek() != -1)
+            {
+                s = g.Readline().Split('_');
+                a.nev = s[0];
+                a.eletkor = int.Parse(s[1]);
+                atl = atl + a.eletkor;
+                sz++;
+            }
+            g.Close();*/
+            //Console.WriteLine("Csoport atlageletkotra: {0}", atl / sz);
+            Console.ReadKey();
+        }
+    }
+}
