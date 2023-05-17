a74cc8ac6af5d5bbeaf034d7a773582599f9a226
szabomarcel <szabomarcell34@gmail.com>
2023-05-17 19:58:38 +0200

    main

---
+++ BaratokOOP/BaratokBuli.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    struct Buli
+    {
+        public string Name; // Szöveges név változó
+        public DateTime szulido; // Dátum változó DateTime-ban
+        public char nem; // Nem változó char-ban
+        public int hajlam;  // Hajlam int-be
+    }
+    internal class BaratokBuli
+    {
+        #region Konstructor
+        public BaratokBuli() { }
+        #endregion Konstructor
+        #region Bulizás
+        public void bulizas()
+        {
+            Console.WriteLine("Kérem adja meg az életkorát:");
+            int E = int.Parse(Console.ReadLine());
+            int a = 0;
+
+            if (E > 0) a = 1;
+            if (E > 7) a = 2;
+            if (E > 19) a = 3;
+            if (E > 65) a = 4;
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("Gyerek");
+                    break;
+                case 2:
+                    Console.WriteLine("Iskolás");
+                    break;
+                case 3:
+                    Console.WriteLine("Felnőtt");
+                    break;
+                case 4:
+                    Console.WriteLine("Nyugdijas");
+                    break;
+                default:
+                    Console.WriteLine("Rossz éréket adott meg.");
+                    break;
+            }
+            Console.ReadLine();
+        }
+        #endregion Bulizás
+    }  
+}
---
+++ BaratokOOP/BaratokScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaratokOOP
+{
+    struct Lista
+    {
+        public string Name; // Szöveges név változó
+        public DateTime szulido; // Dátum változó DateTime-ban
+        public char nem; // Nem változó char-ban
+        public int hajlam;  // Hajlam int-be
+    }
+    internal class BaratokScreen
+    {
+        #region Konstructor
+        public BaratokScreen() { }
+        #endregion Konstructor
+
+        #region Képernyőre listázás
+        public void listazas()
+        {
+            StreamReader olvas = new StreamReader(@"C:\\Users\\Lenovo\\Source\\Repos\\csharp\\BaratOOP\\teszt.csv");
+            string szoveg;
+            while (!olvas.EndOfStream)
+            {
+                szoveg = olvas.ReadLine();
+                Console.WriteLine(szoveg);
+            }
+            olvas.Close();
+        }
+        #endregion Képernyőre listázás
+    }
+}
