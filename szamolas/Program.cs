ad033852a076790b5d7df373f0de2dd72d62fa9d
szabomarcel <szabomarcell34@gmail.com>
2023-06-01 08:47:26 +0200

    main

--- szamolas/Program.cs
+++ szamolas/Program.cs
@@ -10,18 +10,22 @@ namespace szamolas
     {
         static void Main(string[] args)
         {
-            var a = 5;
-            var b = 2;
-            var c = 10;
+            var a = 30;
+            var b = 5;
+            var c = 15;
+            var d = 4;
+            var e = 3;
 
-            Console.Write("Amikor + van:\n");
+            Console.Write("Amikor összeadás van:\t");
             Console.WriteLine(a + b + c);
-            Console.Write("Amikor - van:\n");
+            Console.Write("Amikor kívonás van:\t");
             Console.WriteLine(a + b - c);
-            Console.Write("Amikor * -s van:\n");
+            Console.Write("Amikor szirzás is van:\t");
             Console.WriteLine(a + b * c);
-            Console.Write("Amikor / -s van:\n");
+            Console.Write("Amikor osztás is van:\t");
             Console.WriteLine(a + b / c);
+            Console.Write("Amikor kiszámolás oldunk meg:\t");
+            Console.WriteLine(a / b * (c - d * e);
             Console.WriteLine("A meg adott számok végeredménye.");
             Console.ReadKey();
         }
