bd2f47702bfffbb3ea7fbe7935e50a7a7ae53e5f
szabomarcel <szabomarcell34@gmail.com>
2023-06-07 11:44:14 +0200

    Main

---
+++ 18.9es18.10Halmazok/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace _18._9es18._10Halmazok
+{
+    partial class Szoveg
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LBx_A_B = new TextBox();
+            LBx_B_A = new TextBox();
+            Eloallit = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            LBx_A = new ListBox();
+            LBx_B = new ListBox();
+            LBx_Unio = new ListBox();
+            LBx_Metsz = new ListBox();
+            TBx_A = new ListBox();
+            TBx_B = new ListBox();
+            SuspendLayout();
+            // 
+            // LBx_A_B
+            // 
+            LBx_A_B.Location = new Point(177, 402);
+            LBx_A_B.Name = "LBx_A_B";
+            LBx_A_B.Size = new Size(65, 23);
+            LBx_A_B.TabIndex = 0;
+            // 
+            // LBx_B_A
+            // 
+            LBx_B_A.Location = new Point(248, 402);
+            LBx_B_A.Name = "LBx_B_A";
+            LBx_B_A.Size = new Size(70, 23);
+            LBx_B_A.TabIndex = 1;
+            // 
+            // Eloallit
+            // 
+            Eloallit.Location = new Point(338, 401);
+            Eloallit.Name = "Eloallit";
+            Eloallit.Size = new Size(75, 23);
+            Eloallit.TabIndex = 2;
+            Eloallit.Text = "Előállít";
+            Eloallit.UseVisualStyleBackColor = true;
+            Eloallit.Click += Eloallit_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 15);
+            label1.TabIndex = 5;
+            label1.Text = "A:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(159, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 15);
+            label2.TabIndex = 6;
+            label2.Text = "B:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(281, 68);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 7;
+            label3.Text = "A unio B";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(403, 69);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 15);
+            label4.TabIndex = 8;
+            label4.Text = "A metszet B";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(525, 68);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 15);
+            label5.TabIndex = 9;
+            label5.Text = "A-B";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(647, 68);
+            label6.Name = "label6";
+            label6.Size = new Size(27, 15);
+            label6.TabIndex = 10;
+            label6.Text = "B-A";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(193, 384);
+            label7.Name = "label7";
+            label7.Size = new Size(125, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Halmazok elemszáma:";
+            // 
+            // LBx_A
+            // 
+            LBx_A.FormattingEnabled = true;
+            LBx_A.ItemHeight = 15;
+            LBx_A.Location = new Point(37, 92);
+            LBx_A.Name = "LBx_A";
+            LBx_A.Size = new Size(110, 229);
+            LBx_A.TabIndex = 12;
+            // 
+            // LBx_B
+            // 
+            LBx_B.FormattingEnabled = true;
+            LBx_B.ItemHeight = 15;
+            LBx_B.Location = new Point(159, 92);
+            LBx_B.Name = "LBx_B";
+            LBx_B.Size = new Size(110, 229);
+            LBx_B.TabIndex = 13;
+            // 
+            // LBx_Unio
+            // 
+            LBx_Unio.FormattingEnabled = true;
+            LBx_Unio.ItemHeight = 15;
+            LBx_Unio.Location = new Point(281, 92);
+            LBx_Unio.Name = "LBx_Unio";
+            LBx_Unio.Size = new Size(110, 229);
+            LBx_Unio.TabIndex = 14;
+            // 
+            // LBx_Metsz
+            // 
+            LBx_Metsz.FormattingEnabled = true;
+            LBx_Metsz.ItemHeight = 15;
+            LBx_Metsz.Location = new Point(403, 92);
+            LBx_Metsz.Name = "LBx_Metsz";
+            LBx_Metsz.Size = new Size(110, 229);
+            LBx_Metsz.TabIndex = 15;
+            // 
+            // TBx_A
+            // 
+            TBx_A.FormattingEnabled = true;
+            TBx_A.ItemHeight = 15;
+            TBx_A.Location = new Point(525, 92);
+            TBx_A.Name = "TBx_A";
+            TBx_A.Size = new Size(110, 229);
+            TBx_A.TabIndex = 16;
+            // 
+            // TBx_B
+            // 
+            TBx_B.FormattingEnabled = true;
+            TBx_B.ItemHeight = 15;
+            TBx_B.Location = new Point(647, 92);
+            TBx_B.Name = "TBx_B";
+            TBx_B.Size = new Size(110, 229);
+            TBx_B.TabIndex = 17;
+            // 
+            // Szoveg
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TBx_B);
+            Controls.Add(TBx_A);
+            Controls.Add(LBx_Metsz);
+            Controls.Add(LBx_Unio);
+            Controls.Add(LBx_B);
+            Controls.Add(LBx_A);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Eloallit);
+            Controls.Add(LBx_B_A);
+            Controls.Add(LBx_A_B);
+            Name = "Szoveg";
+            Text = "Halmazok";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox LBx_A_B;
+        private TextBox LBx_B_A;
+        private Button Eloallit;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private ListBox LBx_A;
+        private ListBox LBx_B;
+        private ListBox LBx_Unio;
+        private ListBox LBx_Metsz;
+        private ListBox TBx_A;
+        private ListBox TBx_B;
+    }
+}
