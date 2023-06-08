85f282be1ff1a63c7a7066e33815f1059f8dce00
szabomarcel <szabomarcell34@gmail.com>
2023-06-08 14:49:41 +0200

    main

---
+++ 18.6Kepetforgat/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _18._6Kepetforgat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Lb_7 = new PictureBox();
+            Lb_2 = new PictureBox();
+            Lb_3 = new PictureBox();
+            Lb_4 = new PictureBox();
+            Lb_5 = new PictureBox();
+            Lb_6 = new PictureBox();
+            Lb_1 = new PictureBox();
+            Lb_8 = new PictureBox();
+            colorDialog1 = new ColorDialog();
+            btnJobb = new Button();
+            btnBal = new Button();
+            ((System.ComponentModel.ISupportInitialize)Lb_7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_8).BeginInit();
+            SuspendLayout();
+            // 
+            // Lb_7
+            // 
+            Lb_7.Image = Properties.Resources.letöltés__7_;
+            Lb_7.Location = new Point(154, 167);
+            Lb_7.Name = "Lb_7";
+            Lb_7.Size = new Size(100, 85);
+            Lb_7.TabIndex = 0;
+            Lb_7.TabStop = false;
+            // 
+            // Lb_2
+            // 
+            Lb_2.Image = Properties.Resources.letöltés__1_;
+            Lb_2.Location = new Point(484, 51);
+            Lb_2.Name = "Lb_2";
+            Lb_2.Size = new Size(100, 85);
+            Lb_2.TabIndex = 1;
+            Lb_2.TabStop = false;
+            // 
+            // Lb_3
+            // 
+            Lb_3.Image = Properties.Resources.letöltés__2_;
+            Lb_3.Location = new Point(520, 167);
+            Lb_3.Name = "Lb_3";
+            Lb_3.Size = new Size(100, 85);
+            Lb_3.TabIndex = 2;
+            Lb_3.TabStop = false;
+            // 
+            // Lb_4
+            // 
+            Lb_4.Image = Properties.Resources.letöltés__3_;
+            Lb_4.Location = new Point(457, 286);
+            Lb_4.Name = "Lb_4";
+            Lb_4.Size = new Size(100, 85);
+            Lb_4.TabIndex = 3;
+            Lb_4.TabStop = false;
+            // 
+            // Lb_5
+            // 
+            Lb_5.Image = Properties.Resources.letöltés__5_;
+            Lb_5.Location = new Point(327, 353);
+            Lb_5.Name = "Lb_5";
+            Lb_5.Size = new Size(100, 85);
+            Lb_5.TabIndex = 4;
+            Lb_5.TabStop = false;
+            // 
+            // Lb_6
+            // 
+            Lb_6.Image = Properties.Resources.letöltés__6_;
+            Lb_6.Location = new Point(209, 286);
+            Lb_6.Name = "Lb_6";
+            Lb_6.Size = new Size(100, 85);
+            Lb_6.TabIndex = 5;
+            Lb_6.TabStop = false;
+            // 
+            // Lb_1
+            // 
+            Lb_1.Image = Properties.Resources.letöltés;
+            Lb_1.Location = new Point(327, 12);
+            Lb_1.Name = "Lb_1";
+            Lb_1.Size = new Size(124, 85);
+            Lb_1.TabIndex = 6;
+            Lb_1.TabStop = false;
+            // 
+            // Lb_8
+            // 
+            Lb_8.Image = Properties.Resources.letöltés__4_;
+            Lb_8.Location = new Point(209, 51);
+            Lb_8.Name = "Lb_8";
+            Lb_8.Size = new Size(100, 85);
+            Lb_8.TabIndex = 7;
+            Lb_8.TabStop = false;
+            // 
+            // btnJobb
+            // 
+            btnJobb.Location = new Point(656, 56);
+            btnJobb.Name = "btnJobb";
+            btnJobb.Size = new Size(75, 23);
+            btnJobb.TabIndex = 8;
+            btnJobb.Text = "Jobb";
+            btnJobb.UseVisualStyleBackColor = true;
+            btnJobb.Click += btnJobb_Click;
+            // 
+            // btnBal
+            // 
+            btnBal.Location = new Point(43, 56);
+            btnBal.Name = "btnBal";
+            btnBal.Size = new Size(75, 23);
+            btnBal.TabIndex = 9;
+            btnBal.Text = "Bal";
+            btnBal.UseVisualStyleBackColor = true;
+            btnBal.Click += btnBal_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnBal);
+            Controls.Add(btnJobb);
+            Controls.Add(Lb_8);
+            Controls.Add(Lb_1);
+            Controls.Add(Lb_6);
+            Controls.Add(Lb_5);
+            Controls.Add(Lb_4);
+            Controls.Add(Lb_3);
+            Controls.Add(Lb_2);
+            Controls.Add(Lb_7);
+            Name = "Form1";
+            Text = "Kép forgatás";
+            ((System.ComponentModel.ISupportInitialize)Lb_7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Lb_8).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox Lb_7;
+        private PictureBox Lb_2;
+        private PictureBox Lb_3;
+        private PictureBox Lb_4;
+        private PictureBox Lb_5;
+        private PictureBox Lb_6;
+        private PictureBox Lb_1;
+        private PictureBox Lb_8;
+        private ColorDialog colorDialog1;
+        private Button btnJobb;
+        private Button btnBal;
+    }
+}
---
+++ 18.6Kepetforgat/Form1.cs
@@ -0,0 +1,36 @@
+namespace _18._6Kepetforgat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBal_Click(object sender, EventArgs e)
+        {
+            Image s = Lb_1.Image;
+            Lb_1.Image = Lb_2.Image;
+            Lb_2.Image = Lb_3.Image;
+            Lb_3.Image = Lb_4.Image;
+            Lb_4.Image = Lb_5.Image;
+            Lb_5.Image = Lb_6.Image;
+            Lb_6.Image = Lb_7.Image;
+            Lb_7.Image = Lb_8.Image;
+            Lb_8.Image = s;
+        }
+
+        private void btnJobb_Click(object sender, EventArgs e)
+        {
+            Image s = Lb_8.Image;
+            Lb_8.Image = Lb_7.Image;
+            Lb_7.Image = Lb_6.Image;
+            Lb_6.Image = Lb_5.Image;
+            Lb_5.Image = Lb_4.Image;
+            Lb_4.Image = Lb_3.Image;
+            Lb_3.Image = Lb_2.Image;
+            Lb_2.Image = Lb_1.Image;
+            Lb_1.Image = s;
+        }
+    }
+}
