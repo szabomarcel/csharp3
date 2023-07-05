affa2cc572af4b457e89731b574f09b8bb6f2930
szabomarcel <szabomarcell34@gmail.com>
2023-07-05 20:52:26 +0200

    main

---
+++ ELETKOR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ELETKOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg az életkorát: ");
+            int E = int.Parse(Console.ReadLine());
+            int a = 0;
+            if (E >= 0 && E < 7) a = 1;
+            if (E >= 7 && E < 22) a = 2;
+            if (E >= 19 && E < 66) a = 3;
+            if (E >= 65) a = 4;
+            switch (a)
+            {
+                case 1: Console.WriteLine("Gyermek."); break;
+                case 2: Console.WriteLine("Iskolás."); break;
+                case 3: Console.WriteLine("Felnőtt."); break;
+                case 4: Console.WriteLine("Nyugdíjas."); break;
+                default: Console.WriteLine("Rossz értéket adott meg."); break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ matrixa/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace matrixa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matr = new int[2, 2]; // Két dimenziós mátrix tömb. 
+            for (int i = 0; i < 2; i++) // For ciklu az i-edik elemre
+            {
+                for (int j = 0; j < 2; j++) // For ciklu az j-edik elemre
+                {
+                    Console.Write("Kérem az {0}. sor {1}. elemét: ", i + 1, j + 1);
+                    matr[i, j] = int.Parse(Console.ReadLine()); // Mátrix konvertálás
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("A matrix: ");
+            Console.WriteLine();
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("{0}     ", matr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            int det = matr[0, 0] * matr[1, 1] - matr[0, 1] * matr[1, 0];
+            Console.WriteLine("A mátrix determinánsa: {0}", det);
+            Console.WriteLine();
+        }
+    }
+}
