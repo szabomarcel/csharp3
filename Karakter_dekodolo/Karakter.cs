3698cd0412453c46d4d574a167b3ddac56d8d82f
szabomarcel <szabomarcell34@gmail.com>
2023-09-10 21:26:56 +0200

    main

---
+++ Karakter_dekodolo/Karakter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karakter_dekodolo
+{
+    internal class Karakter
+    {
+        public string karakter;
+        public string matrix;
+
+        public Karakter(string karakter, string matrix)
+        {
+            this.karakter = karakter;
+            this.matrix = matrix;
+        }
+        public static void kiir(Karakter valami)
+        {
+            int j = 0;
+            foreach(var i in valami.matrix)
+            {
+                if (i == '1')
+                {
+                    Console.Write("X");
+                }
+                else
+                    Console.Write(" ");
+            }
+            j++;
+            if(j % 4 == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        public static bool Felismer(Karakter kod, Karakter kodolando)
+        {
+            bool ismer = false;
+            if (kod.matrix == kodolando.matrix)
+            {
+                ismer = true;
+            }
+            return ismer; 
+        }
+
+    }
+}
---
+++ Karakter_dekodolo/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace Karakter_dekodolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string karakter ; string matrix = "";
+            string[] beolvas = File.ReadAllLines("bank.txt");
+            List<Karakter> adatok = new List<Karakter>();
+            for (int i = 0; i < beolvas.Length; i+=8)
+            {
+                karakter = beolvas[i];
+                for (int j = 1; j < 8; j++)
+                {
+                    matrix += beolvas[i + j];
+                }
+                Karakter resz = new Karakter(karakter, matrix);
+                adatok.Add(resz);
+                matrix = "";
+            }
+            Console.WriteLine($"Karakterek száma: {adatok.Count}");
+
+            foreach(Karakter resz in adatok)
+            {
+                Console.WriteLine(resz.karakter + " " + resz.matrix);
+            }
+            Console.WriteLine("6. feladat");
+            List<string> abc = new List<string>();
+            for (char i = 'A'; i <= 'Z' ; i++)
+            {
+                abc.Add(i.ToString());
+            }
+            string be = "";
+
+            while (!abc.Contains(be))
+            {
+                Console.WriteLine("Adj meg egy angol nagybetűt: ");
+                be = Console.ReadLine();
+            }
+            Console.WriteLine("7.Feladat");
+            bool van = false;
+            foreach (var i in adatok)
+            {
+                if(be == i.karakter)
+                {
+                    Karakter.kiir(i);
+                    van = true;
+                }
+            }
+            if(!van)
+            {
+                Console.WriteLine("Nincs ilyen karakter!");
+            }
+
+            string[] ujbeolvas = File.ReadAllLines("dekodol.txt");
+            List<Karakter> ujadatok = new List<Karakter>();
+            for (int i = 0; i < ujbeolvas.Length; i += 8)
+            {
+                karakter = ujbeolvas[i];
+                for (int j = 1; j < 8; j++)
+                {
+                    matrix += ujbeolvas[i + j];
+                }
+                Karakter resz = new Karakter(karakter, matrix);
+                ujadatok.Add(resz);
+                matrix = "";
+            }
+
+            string titok = "";
+            bool ismert = false;
+            for (int i = 0; i < ujadatok.Count; i++)
+            {
+                for (int j = 0; j < adatok.Count; j++)
+                {
+                    if (Karakter.Felismer(adatok[j], ujadatok[i]))
+                    {
+                        ismert = true;
+                        titok += adatok[j];
+                    }
+                    if (!ismert)
+                    {
+                        titok += "?";
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
