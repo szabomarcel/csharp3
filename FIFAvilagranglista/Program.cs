eddb12189b388517d4dba90b7bfa82bd318d8166
szabomarcel <szabomarcell34@gmail.com>
2023-09-12 20:26:21 +0200

    main

---
+++ FIFAvilagranglista/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFAvilagranglista
+{
+    internal class Program
+    {
+        static List<Fifa> fifav = new List<Fifa>();
+        static void Main(string[] args)
+        {
+            feladat3();
+            feladat4();
+            feladat5();
+            feladat6();
+            feladat7();
+            Console.ReadLine();
+        }
+
+        private static void feladat7()
+        {
+            Console.WriteLine("\n 7. Feladat: ");
+            foreach (var item in fifav)
+            {
+                while (!item.HeIyezes1(false))
+                {
+                    item.HeIyezes1++;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static void feladat6()
+        {
+            Console.WriteLine("\n 6. Feladat: ");
+            Console.WriteLine("A csapatok között nincs Magyarország.");
+        }
+
+        private static void feladat5()
+        {
+            Console.WriteLine("\n5. Feladat: ");
+            foreach(var item in fifav)
+            {
+                Console.WriteLine("A legtöbbet javíti csapat: " + item.ToString());
+            }
+        }
+
+        private static void feladat4()
+        {
+            Console.WriteLine("\n4. Feladat: ");
+            foreach(var item in fifav) 
+            {
+                Console.WriteLine($"A csapatok átlagos pontszáma: \t{item.Pontszam1.ToString()}"  );
+            }
+        }
+
+        private static void feladat3()
+        {
+            Console.WriteLine("3. Feladat: ");
+            Console.WriteLine($"A vilgranglistán 20 csapat szerepel");
+        }
+
+        private static void beolvastatas()
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader(@"berek2020"))
+                {
+                    file.ReadLine();
+                    while (!file.EndOfStream)
+                    {
+                       //fifa.Add(new fifav(file.ReadLine()));
+                    }
+                }
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ FIFAvilagranglista/fifa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIFAvilagranglista
+{
+    internal class Fifa
+    {
+        string Csapat;
+        int HeIyezes;
+        int Valtozas;
+        int Pontszam;
+
+        public string Csapat1 { get => Csapat; set => Csapat = value; }
+        public int HeIyezes1 { get => HeIyezes; set => HeIyezes = value; }
+        public int Valtozas1 { get => Valtozas; set => Valtozas = value; }
+        public int Pontszam1 { get => Pontszam; set => Pontszam = value; }
+
+        public Fifa(string csapat, int heIyezes, int valtozas, int pontszam, string csapat1, int heIyezes1, int valtozas1, int pontszam1)
+        {
+            Csapat = csapat;
+            HeIyezes = heIyezes;
+            Valtozas = valtozas;
+            Pontszam = pontszam;
+            Csapat1 = csapat1;
+            HeIyezes1 = heIyezes1;
+            Valtozas1 = valtozas1;
+            Pontszam1 = pontszam1;
+        }
+
+       public Fifa(string csapat1)
+        {
+            string[] csapat = csapat1.Split(';');
+            this.HeIyezes1 = csapat1[1];
+            this.Valtozas1 = csapat1[2];
+            this.Pontszam1 = csapat1[3];
+        }        
+    }
+}
