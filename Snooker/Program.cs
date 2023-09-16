520ccda985ec6eb00e1ec0d14e0cc4d5119a2f60
szabomarcel <szabomarcell34@gmail.com>
2023-09-16 17:44:39 +0200

    main

---
+++ Snooker/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snooker
+{
+    internal class Program
+    {
+        static List<Snook> smooker = new List<Snook>();
+        static void Main(string[] args)
+        {
+            feladat3();
+            feladat4();
+            Console.ReadLine();
+        }
+
+        private static void feladat4()
+        {
+            Console.WriteLine("\n4. Feladat: ");
+            Console.WriteLine($"A versenyzők átlagosan {} fontot keresnek.");
+        }
+
+        private static void feladat3()
+        {
+            Console.WriteLine("3. Feladat: ");
+            object Helyezes = null;
+            Console.WriteLine($"A világranglista {Helyezes} versenyző szerepel.");
+        }
+
+        private static void beolvastatas()
+        {
+            try
+            {
+                using (StreamReader file = new StreamReader("smooker"))
+                {
+                    file.ReadLine();
+                    while (!file.EndOfStream)
+                    {
+                        smooker.Add(new Snook(file.ReadLine()));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Snooker/snook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snooker
+{
+    internal class Snook
+    {
+        int Helyezes;
+        string Nev;
+        string Orszag;
+        int Nyeremeny;
+
+        public int Helyezes1 { get => Helyezes; set => Helyezes = value; }
+        public string Nev1 { get => Nev; set => Nev = value; }
+        public string Orszag1 { get => Orszag; set => Orszag = value; }
+        public int Nyeremeny1 { get => Nyeremeny; set => Nyeremeny = value; }
+
+        public Snook(int helyezes, string nev, string orszag, int nyeremeny, int helyezes1, string nev1, string orszag1, int nyeremeny1)
+        {
+            Helyezes = helyezes;
+            Nev = nev;
+            Orszag = orszag;
+            Nyeremeny = nyeremeny;
+            Helyezes1 = helyezes1;
+            Nev1 = nev1;
+            Orszag1 = orszag1;
+            Nyeremeny1 = nyeremeny1;
+        }
+        public Snook(string beolvastatottSOr)
+        {
+            string[] strings = beolvastatottSOr.Split(';');
+            Helyezes = int.Parse(strings[0]);
+            Nev = strings[1];
+            Orszag = strings[2];
+            Nyeremeny = int.Parse(strings[3]);
+        }
+    }
+}
