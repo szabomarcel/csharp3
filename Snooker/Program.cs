41caac8d82e2ae9e413ef85fde6803dde7852499
szabomarcel <szabomarcell34@gmail.com>
2023-09-17 20:54:33 +0200

    feladat

--- Snooker/Program.cs
+++ Snooker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,20 +16,52 @@ namespace Snooker
         {
             feladat3();
             feladat4();
+            feladat5();
+            feladat6();
+            feladat7();
             Console.ReadLine();
+        }
+
+        private static void feladat7()
+        {
+            Console.WriteLine("\t7.feladat: Statisztika");
+            foreach (var item in smooker.FindAll(z => z.Helyezes1 == 0).GroupBy(a => a.Nev1).Select(b => new { ok = b.Key, db = b.Count() }))
+            {
+                if (item.db > 2)
+                {
+                    Console.WriteLine($"\t{item.ok} : {item.db}");
+                }
+            }
+        }
+
+        private static void feladat6()
+        {
+            Console.WriteLine("\n6. Feladat: ");
+            
+            Console.WriteLine($"A versenyzők között van  versenyző");
+        }
+
+        private static void feladat5()
+        {
+            Console.WriteLine("\n5. Feladat: ");
+            /*foreach (Snook item in smooker.FindAll(a => a.Helyezes1).FindAll(b => b.Nev1).FindAll(c => c.Orsazg1).GroupBy(d => d.Nyeremeny1)
+            {
+
+                Console.WriteLine("A legjobb kereső kínai versenyző: ");
+            }*/
         }
 
         private static void feladat4()
         {
             Console.WriteLine("\n4. Feladat: ");
-            Console.WriteLine($"A versenyzők átlagosan {} fontot keresnek.");
+            foreach (Snook item in smooker.FindAll(a => a.Nyeremeny1 == 0).GroupBy(b => b.Helyezes1))            
+                Console.WriteLine($"\tA versenyzők átlagosan {item} fontot keresnek.");            
         }
 
         private static void feladat3()
         {
             Console.WriteLine("3. Feladat: ");
-            object Helyezes = null;
-            Console.WriteLine($"A világranglista {Helyezes} versenyző szerepel.");
+            Console.WriteLine($"A világranglista {smooker.Count} versenyző szerepel.");
         }
 
         private static void beolvastatas()
---
+++ bekertnev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace bekertnev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem, adja meg a nevét: ");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Üdv, " + nev + "!");
+            string fajlnev = "adatok.txt";
+            if(File.Exists(fajlnev))
+            {
+                string[] sorok = File.ReadAllLines(fajlnev);
+                foreach(string sor in sorok)
+                {
+                    Console.WriteLine(sor);
+                }
+            }
+            else
+            {
+                Console.WriteLine("A fájl nem található.");
+            }
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Első parancssori argumentum: " + args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Nincsenek parancssori argumentumok.");
+            }
+        }
+    }
+}
